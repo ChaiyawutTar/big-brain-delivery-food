0610c527fb8359c4e0659c6ad7474c016010cd4c
Chanathip <bipoeman12@gmail.com>
2022-11-28 01:02:37 +0700

    Stock add and view

--- adminConsole.py
+++ adminConsole.py
@@ -1,43 +1,95 @@
 import os
+import json
 shopStatus = 'on'
+
+def greenText(text : str):
+    return "\x1B[38;2;0;200;51m"+ text +"\x1b[0m"
+
 while True:
-    command : str = input("admin@big-brain: ")
+    command : str = input("admin@bigBrainCompany: ")
 
     if command.strip().lower() == 'exit':
         print('bye...')
         break
 
-    if command.lower().strip() == 'clear':
-        os.system('cls')
-
     commandStrip = command.split(" ")
     try:
         if (commandStrip[0].lower().strip() == 'stock'):
-            print("Stock related command here")
+            # print("Stock related command here")
 
             if (commandStrip[1].lower().strip() == 'add'):
-                print("Stock add")
+                if '"' in command:
+                    print("Extract Menu Name")
+                    quotationIndex = command.index('"')
+                    menuName = command[quotationIndex + 1:-1]
+                    menuName = menuName[0:menuName.index('"')]
+                    number = commandStrip[-1]
+                    print(f'"${menuName}"',number)
+                    fileReadText = ''
 
-            if (commandStrip[1].lower().strip() == 'remove'):
-                print("Stock remove")
+                    with open('stock.json','r') as file:
+                        fileReadText = json.load(file)
+                        fileReadText.update({menuName:number})
+
+                    # print(fileReadText)
+                    with open('stock.json','w') as file:
+                        json.dump(fileReadText,file,indent = 4)
+                        print(f'Current Stock : {fileReadText}')
+                        
+                    print("Stock add")
+                else:
+                    print("Stock add invalid")
+                    raise
+
+            elif (commandStrip[1].lower().strip() == 'remove'):
+                if '"' in command:
+                    print("Extract Menu Name")
+                    quotationIndex = command.index('"')
+                    menuName = command[quotationIndex + 1:-1]
+                    menuName = menuName[0:menuName.index('"')]
+
+                    print(menuName)
+                else:
+                    print("Stock remove invalid")
+                    raise
             
-            if (commandStrip[1].lower().strip() == 'view'):
+            elif (commandStrip[1].lower().strip() == 'view'):
                 # Load Stock JSON
-                print("Stock View")
+                with open('stock.json','r') as file:
+                    fileReadText : dict = json.load(file)
+                    stock = fileReadText.items()
+                    print(greenText("\nCurrent Stock:"))
+                    for item in stock:
+                        print(f"\t{item[0]}\t:\t{item[1]}")
+                    print()
+                    # print(f'Current Stock : {fileReadText}')
+                # print("Stock View")
 
-        if (commandStrip[0].lower().strip() == 'shop'):
-            print("Stock related command here")
+
+        elif (commandStrip[0].lower().strip() == 'shop'):
+            # print("Shop related command here")
 
             if (commandStrip[1].lower().strip() == 'on'):
                 shopStatus = 'on'
                 print("Shop ON")
 
             if (commandStrip[1].lower().strip() == 'off'):
+                shopStatus = 'off'
                 print("Shop Off")
             
             if (commandStrip[1].lower().strip() == 'status'):
                 # load shop status
-                print("Stock View")
-                
-    except Exception as e:
-        print(e.message)
+                print(f"Shop status {shopStatus}")
+        elif (command == ""):
+            pass
+        else:
+            print("Unknown command")
+        
+        
+    except:
+        print("Invalid Command")
+        pass
+        # print(e)
+
+    if command.lower().strip() == 'clear':
+        os.system('cls')   
