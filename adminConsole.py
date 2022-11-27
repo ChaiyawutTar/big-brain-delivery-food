99feb6b3ed0d448a4f31b89d895b44b56a969dd5
Chanathip <bipoeman12@gmail.com>
2022-11-27 23:15:39 +0700

    stock and shop command update

--- adminConsole.py
+++ adminConsole.py
@@ -1,4 +1,5 @@
 import os
+shopStatus = 'on'
 while True:
     command : str = input("admin@big-brain: ")
 
@@ -8,3 +9,35 @@ while True:
 
     if command.lower().strip() == 'clear':
         os.system('cls')
+
+    commandStrip = command.split(" ")
+    try:
+        if (commandStrip[0].lower().strip() == 'stock'):
+            print("Stock related command here")
+
+            if (commandStrip[1].lower().strip() == 'add'):
+                print("Stock add")
+
+            if (commandStrip[1].lower().strip() == 'remove'):
+                print("Stock remove")
+            
+            if (commandStrip[1].lower().strip() == 'view'):
+                # Load Stock JSON
+                print("Stock View")
+
+        if (commandStrip[0].lower().strip() == 'shop'):
+            print("Stock related command here")
+
+            if (commandStrip[1].lower().strip() == 'on'):
+                shopStatus = 'on'
+                print("Shop ON")
+
+            if (commandStrip[1].lower().strip() == 'off'):
+                print("Shop Off")
+            
+            if (commandStrip[1].lower().strip() == 'status'):
+                # load shop status
+                print("Stock View")
+                
+    except Exception as e:
+        print(e.message)
