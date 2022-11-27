85337b16f8ccb2da0dac8839bc790df71adb64c7
Chaiyawut THENGKET <chaiyawut.t@ku.th>
2022-11-28 01:05:53 +0700

    wtf

---
+++ stock.py
@@ -0,0 +1,28 @@
+import json
+
+class Stock:
+    def __init__(self, ):
+        self.dict_stock = {}
+        self.ordinary = ordinary
+
+
+    def update_stock(self):
+        new_data = {
+
+        }
+        with open('stock.json', 'r') as f:
+            data =  json.load(f)
+            data.update(new_data)
+        with open('stock.json', 'w') as f:
+            json.dump(data , f, indent = 4)
+
+        # with open("stock.csv", "r") as f:
+        #     data = csv.DictReader
+        
+    def get_menulist(self):
+        pass
+
+    def get_amount(self):
+        pass
+    
+
